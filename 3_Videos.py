d47ad58710185c1e629b307f629616c047a846bf
bian321 <42202295+bian321@users.noreply.github.com>
2026-05-02 21:50:25 +0300

    Update 3_Videos.py

--- 3_Videos.py
+++ 3_Videos.py
@@ -2,12 +2,10 @@ import streamlit as st
 
 st.title("🎥 المكتبة المرئية والمسموعة")
 
-st.header("1. مقدمة شاملة (فيديو)")
-# حطي رابط أي فيديو شرح برولوج بتحسيه ممتاز
-st.video("https://www.youtube.com/watch?v=your_video_id") 
+st.header("1. مقدمة عن البرولوج")
+st.video("https://app.steve.ai/video/3ZS4I0K4GHMKFO0O") 
 
 st.divider()
 
-st.header("2. بودكاست: لماذا البرولوج؟")
-# لو عندك ملف صوتي أو رابط بودكاست
+st.header("2. بودكاست: البرولوج")
 st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
