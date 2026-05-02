4c84779460c54c6474aee9874f74e7d1661cbd52
bian321 <42202295+bian321@users.noreply.github.com>
2026-05-02 22:47:09 +0300

    Update Videos_3.py

--- main_folder/pages/Videos_3.py
+++ main_folder/pages/Videos_3.py
@@ -7,5 +7,12 @@ st.video("https://app.steve.ai/video/3ZS4I0K4GHMKFO0O")
 
 st.divider()
 
-st.header("2. بودكاست: لماذا البرولوج؟")
+st.header("🎧 بودكاست تعليمي")
+
+# الرابط المباشر اللي جهزناه
 podcast_url = "https://drive.google.com/uc?export=download&id=1wAQRGhUfzUDVTGh2jxilhj2RPcHZ4HyX"
+
+# تشغيل البودكاست مباشرة
+st.audio(podcast_url)
+
+st.write("في هذا البودكاست، نناقش أهمية لغة البرولوج في الذكاء الاصطناعي.")
