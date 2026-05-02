822861f0b49b7a0228e147ce0831df49bc0e5755
bian321 <42202295+bian321@users.noreply.github.com>
2026-05-02 23:23:36 +0300

    Update 03_Videos.py

--- main_folder/pages/03_Videos.py
+++ main_folder/pages/03_Videos.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import os
+import streamlit.components.v1 as components
+
+
+st.header("🎧 بودكاست تعليمي")
+
+
+# تضمين مشغل Cloudinary
+cloudinary_embed_url = "https://player.cloudinary.com/embed/?cloud_name=dnaaujzam&public_id=audio1_q73mmw"
+
+components.iframe(cloudinary_embed_url, height=100)
+st.write("في هذا البودكاست، نناقش أهمية لغة البرولوج في الذكاء الاصطناعي.")
+
 assets_path = "main_folder/assets" 
 
 st.title("🎥 المكتبة المرئية والمسموعة")
@@ -14,14 +26,3 @@ else:
     # هاد السطر رح يساعدك تعرفي وين بايثون عم بيدوّر بالظبط لو لسه فيه مشكلة
     st.error(f"الملف غير موجود في: {video_file_path}")
 
-import streamlit.components.v1 as components
-
-
-st.header("🎧 بودكاست تعليمي")
-
-
-# تضمين مشغل Cloudinary
-cloudinary_embed_url = "https://player.cloudinary.com/embed/?cloud_name=dnaaujzam&public_id=audio1_q73mmw"
-
-components.iframe(cloudinary_embed_url, height=100)
-st.write("في هذا البودكاست، نناقش أهمية لغة البرولوج في الذكاء الاصطناعي.")
