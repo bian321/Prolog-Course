338f988d83ca39f65fe7ae31d953a0f3bb95a0b1
bian321 <42202295+bian321@users.noreply.github.com>
2026-05-02 22:39:09 +0300

    Update Videos_3.py

--- main_folder/pages/Videos_3.py
+++ main_folder/pages/Videos_3.py
@@ -8,4 +8,4 @@ st.video("https://app.steve.ai/video/3ZS4I0K4GHMKFO0O")
 st.divider()
 
 st.header("2. بودكاست: لماذا البرولوج؟")
-st.audio("https://notebooklm.google.com/notebook/349b0d86-e2a3-436e-bc26-971116ed3f77/artifact/376e8e35-b347-42c3-b048-ea787a43dc35?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_")
+st.audio("https://drive.google.com/file/d/1wAQRGhUfzUDVTGh2jxilhj2RPcHZ4HyX/view?usp=sharing")
